cccb9912b815e4d44b9e23e40eaf8fc268c5a4b2
Natalie Tan <natalieytan@gmail.com>
2022-12-09 21:18:54 +1100

    Refactor day 9 to generic function

--- internal/day9/day9.go
+++ internal/day9/day9.go
@@ -1,47 +1,32 @@
 package day9
 
 func Part1(motions []Motion) int {
-	tailVisited := map[string]bool{}
-	headCoordinate := NewCoordinate()
-	tailCoordinate := NewCoordinate()
-
-	for _, motion := range motions {
-		for i := 0; i < motion.Distance; i++ {
-			headCoordinate.MoveInDirection(motion.Direction)
-			tailCoordinate.MoveToTouchCoordinate(headCoordinate)
-			tailVisited[tailCoordinate.String()] = true
-		}
-	}
-
-	return len(tailVisited)
+	return tailPositionsVisitedWithKnots(motions, 2)
 }
 
 func Part2(motions []Motion) int {
+	return tailPositionsVisitedWithKnots(motions, 10)
+}
+
+func tailPositionsVisitedWithKnots(motions []Motion, knots int) int {
 	tailVisited := map[string]bool{}
-	headCoordinate := NewCoordinate()
-	t1 := NewCoordinate()
-	t2 := NewCoordinate()
-	t3 := NewCoordinate()
-	t4 := NewCoordinate()
-	t5 := NewCoordinate()
-	t6 := NewCoordinate()
-	t7 := NewCoordinate()
-	t8 := NewCoordinate()
-	t9 := NewCoordinate()
+	rope := make([]*Coordinate, knots)
+	for i := range rope {
+		rope[i] = NewCoordinate()
+	}
 
 	for _, motion := range motions {
 		for i := 0; i < motion.Distance; i++ {
-			headCoordinate.MoveInDirection(motion.Direction)
-			t1.MoveToTouchCoordinate(headCoordinate)
-			t2.MoveToTouchCoordinate(t1)
-			t3.MoveToTouchCoordinate(t2)
-			t4.MoveToTouchCoordinate(t3)
-			t5.MoveToTouchCoordinate(t4)
-			t6.MoveToTouchCoordinate(t5)
-			t7.MoveToTouchCoordinate(t6)
-			t8.MoveToTouchCoordinate(t7)
-			t9.MoveToTouchCoordinate(t8)
-			tailVisited[t9.String()] = true
+			for knotNumber, knot := range rope {
+				if knotNumber == 0 {
+					knot.MoveInDirection(motion.Direction)
+				} else {
+					knot.MoveToTouchCoordinate(rope[knotNumber-1])
+				}
+				if knotNumber == len(rope)-1 {
+					tailVisited[knot.String()] = true
+				}
+			}
 		}
 	}
 
